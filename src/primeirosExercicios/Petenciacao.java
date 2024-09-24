39ff7240baf943feabc93cb58cf3ea22cb663e0b
Erick Lemos <ericklemos.dev@gmail.com>
2024-09-24 18:08:50 -0300

    potenciacao com a classe Math

---
+++ src/primeirosExercicios/Petenciacao.java
@@ -0,0 +1,20 @@
+package primeirosExercicios;
+
+import java.util.Scanner;
+
+public class Petenciacao {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		double val;
+		
+		System.out.println("Digite um valor: ");
+		val = read.nextDouble();
+		
+		System.out.printf("O valor %.2f elevado ao quadrado é: %.2f\n", val, Math.pow(val, 2));
+		System.out.printf("O valor %.2f elevado ao cubo é: %.2f\n", val, Math.pow(val, 3));
+		
+
+	}
+
+}
