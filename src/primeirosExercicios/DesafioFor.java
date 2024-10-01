949a147e2304c2f8b8d1d3e608df6251dc387c6e
Erick Lemos <ericklemos.dev@gmail.com>
2024-10-01 19:29:38 -0300

    Controlando for com string

---
+++ src/primeirosExercicios/DesafioFor.java
@@ -0,0 +1,12 @@
+package primeirosExercicios;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+		//String valor = "#";
+		
+		for(String valor = "#"; !valor.equals("######") ; valor += "#") {
+			System.out.println(valor);
+			
+		}
+	}
+}
